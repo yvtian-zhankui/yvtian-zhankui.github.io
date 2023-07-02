d8cb75d0de7c237fe1041050a043f5505320ff01
老振 <1787774812@qq.com>
2023-07-02 18:56:32 +0800

    doc

--- upPress.sh
+++ upPress.sh
@@ -14,18 +14,22 @@ echo 'themoss.eu.org' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
-git push -f git@github.com:yvtian-zhankui/yvtian-zhankui.github.io.git master:gh-pages
+git commit -m 'web'
+git push -f git@github.com:yvtian-zhankui/yvtian-zhankui.github.io.git master:pages
 
 
-cd ../../../
+cd -
 
 git add -A
-git commit -m 'deploy'
-git push -f
+git commit -m 'doc'
+git push -f git@github.com:yvtian-zhankui/yvtian-zhankui.github.io.git 
+
+
+rm -rf docs/.vitepress/dist  #删除dist文件夹
+
 
 # 如果部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:yvtian-zhankui/yvtian-zhankui.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
