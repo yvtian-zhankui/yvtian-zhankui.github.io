10c098c83c80f742b738f4a0f57004c8808a5339
老振 <1787774812@qq.com>
2023-07-05 16:48:28 +0800

    doc

--- upPress.sh
+++ upPress.sh
@@ -11,8 +11,8 @@ cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 echo 'themoss.eu.org' > CNAME
-
-git init
+echo 'a16c972235862718150cccb5851babdb6b3d63c5' > 016bea2a2201ed15e394ce2848620b25.txt
+# git init
 git add -A
 git commit -m 'web'
 git push -f git@github.com:yvtian-zhankui/yvtian-zhankui.github.io.git master:pages
@@ -25,7 +25,7 @@ git commit -m 'doc'
 git push -f git@github.com:yvtian-zhankui/yvtian-zhankui.github.io.git 
 
 
-rm -rf docs/.vitepress/dist  #删除dist文件夹
+# rm -rf docs/.vitepress/dist  #删除dist文件夹
 
 
 # 如果部署到 https://<USERNAME>.github.io
